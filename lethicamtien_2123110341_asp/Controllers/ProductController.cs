feb8930db5a08aa60b66a49e89008c45371b1956
HP <HP@HP>
2026-03-12 08:04:21 +0700

    push

---
+++ lethicamtien_2123110341_asp/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lethicamtien_2123110341_asp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
