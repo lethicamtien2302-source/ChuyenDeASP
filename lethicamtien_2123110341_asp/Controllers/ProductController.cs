7ed4bfe8451b60f7c7f7d970c6ba8d97c3f73a8d
HP <HP@HP>
2026-03-12 08:49:53 +0700

    put2

--- lethicamtien_2123110341_asp/Controllers/ProductController.cs
+++ lethicamtien_2123110341_asp/Controllers/ProductController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace lethicamtien_2123110341_asp.Controllers
 {
@@ -7,6 +6,48 @@ namespace lethicamtien_2123110341_asp.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        static List<string> products = new List<string>()
+        {
+            "Apple",
+            "Banana"
+        };
 
+        // GET: api/Product
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+
+        // GET: api/Product/1
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(products[id]);
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            products.Add(name);
+            return Ok(products);
+        }
+
+        // PUT: api/Product/1
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, string name)
+        {
+            products[id] = name;
+            return Ok(products);
+        }
+
+        // DELETE: api/Product/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            products.RemoveAt(id);
+            return Ok(products);
+        }
     }
 }
